c2a646f9a23eeeb1b43f2d521a5e0242c88fe61f
IKAI5 <egoism135@gmail.com>
2026-06-05 16:03:29 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAPJi86QkQqaoKSTos8St9l8GVseaJthsY",
+  authDomain: "mine-65fe9.firebaseapp.com",
+  databaseURL: "https://mine-65fe9-default-rtdb.firebaseio.com",
+  projectId: "mine-65fe9",
+  storageBucket: "mine-65fe9.firebasestorage.app",
+  messagingSenderId: "832545743280",
+  appId: "1:832545743280:web:5d4f2f33476bcc11a1f675",
 };
